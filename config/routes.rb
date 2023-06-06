a72e0ca8987d83b32f5574ef178a0f0c4a7a249f
HanaHarada <129660325+hana5555@users.noreply.github.com>
2023-06-06 07:16:44 +0000

    [Add]route記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,79 +1,42 @@
 Rails.application.routes.draw do
 
-  namespace :admin do
-    get 'messages/index'
-    get 'messages/new'
-    get 'messages/create'
-    get 'messages/edit'
-    get 'messages/update'
-    get 'messages/destroy'
-  end
-  namespace :admin do
-    get 'categories/index'
-    get 'categories/create'
-    get 'categories/edit'
-    get 'categories/update'
-  end
-  namespace :admin do
-    get 'users/index'
-    get 'users/show'
-    get 'users/edit'
-    get 'users/update'
-  end
-  namespace :admin do
-    get 'comments/index'
-    get 'comments/show'
-    get 'comments/update'
-  end
-  namespace :admin do
-    get 'posts/index'
-    get 'posts/show'
-    get 'posts/update'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  namespace :public do
-    get 'searches/search'
-  end
-  namespace :public do
-    get 'favorites/create'
-    get 'favorites/destroy'
-  end
-  namespace :public do
-    get 'comments/create'
-    get 'comments/destroy'
-  end
-  namespace :public do
-    get 'users/show'
-    get 'users/edit'
-    get 'users/update'
-    get 'users/unsubscribe'
-    get 'users/withdrawal'
-  end
-  namespace :public do
-    get 'posts/index'
-    get 'posts/new'
-    get 'posts/create'
-    get 'posts/show'
-    get 'posts/edit'
-    get 'posts/update'
-    get 'posts/destroy'
-  end
-  namespace :public do
-    get 'homes/top'
-  end
-  #ユーザー用
+
+#ユーザー用
   #URL /user/sign_in ...
   devise_for :users,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
 
-  #管理者用
+  #退会確認画面
+  get '/users/:id/unsubscribe' => 'public/users#unsubscribe', as: 'unsubscribe'
+  #論理削除用のルーティング
+  patch '/users/:id/withdrawal' => 'public/users#withdrawal', as: 'withdrawal'
+
+  root to: 'public/homes#top'
+  scope module: :public do
+    resources :posts, only:[:index, :new, :create, :show, :edit, :update, :destroy]
+    resources :users, only:[:show, :edit, :update, :unsubscribe, :withdrawal]
+    resources :comments, only:[:create, :destroy]
+    resources :favorites, only:[:create, :destroy]
+    resources :searches, only:[:search]
+  end
+
+
+#管理者用
   #URL /admin/sign_in ...
   devise_for :admin,skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
+
+  get '/admin' => 'admin/homes#top'
+  namespace :admin do
+    resources :posts, only:[:index, :show, :update]
+    resources :comments, only:[:index, :show, :update]
+    resources :users, only:[:index, :show, :edit, :update]
+    resources :categories, only:[:index, :create, :edit, :update]
+    resources :messages, only:[:index, :new, :create, :edit, :update, :destroy]
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
